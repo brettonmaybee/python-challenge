b48e11083b6bf7763b79b619804941c85c14843b
brettonmaybee <brettonmaybee@users.noreply.github.com>
2020-09-08 16:09:58 -0700

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,36 +4,62 @@ import csv
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
     
-with open(csvpath) as csvfile:
-    
-    row_count =sum(1 for row in csvfile)
-
-print (f"Total Months: {row_count-1}")
 
 with open(csvpath) as csvfile:
 
     csvreader=csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
-
     csv_header=next(csvreader)
 
-    profits_losses=[int()]
+    #total_months =sum(1 for row in csvfile)
+    total_months=len(csvreader)
+    
+ 
+with open(csvpath) as csvfile:
+
+    csvreader=csv.reader(csvfile, delimiter=',')
+
+    csv_header=next(csvreader)   
+    
+    profits_losses=[]
 
     for row in csvreader: 
         profits_losses.append(row[1])
 
-profits_losses=[int(i) for i in profits_losses]
+p_l_int=[int(i) for i in profits_losses]
 
 def sum(data):
     total=0
-    for row in data:
-        total += row
+    for x in data:
+        total += x
     return total
 
-total=sum(profits_losses)
-
-print(total)
+total=sum(p_l_int)
 
 
+def mean(data):
+    total=0
+    for x in data:
+        total +=x
+        average=total/len(data)
+    return average
 
+average=mean(p_l_int)
+
+
+def change(data):
+    total=0
+    for x in data:
+        total +=x
+        mean=total/len(data)
+        data[:] = [i - mean for i in data]
+    return data
+
+
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {total_months}")"
+print(f"Total: ${total}")
+print(f"Average  Change: ${avg_change}")
+print(f"Greatest Increase in Profits: {max_row[0]} ${max_row[1]}")
+print(f"Greatest Decrease in Profits: {min_row[0]} ${min_row[1]}")
