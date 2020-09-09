92461d8464991ada9d918f1fd0d61b7764261081
brettonmaybee <brettonmaybee@users.noreply.github.com>
2020-09-08 20:47:17 -0700

    Functioning Code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,11 +12,11 @@ with open(csvpath) as csvfile:
 
     print(csvreader)
 
-    # Read the header row first (skip this step if there is now header)
+    # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
     #for row in csvreader:
-     #   print(row)
+    #   print(row)
 
